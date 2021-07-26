0262d63eadb22d0765da51cc8f94310ef18d55e1
pakizheng <pakizheng@tencent.com>
2021-07-26 21:08:28 +0800

    chore: 功能完善

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 const through = require('through2')
 const path = require('path')
 const fs = require('fs')
+const fsExtra = require('fs-extra')
+
+const commonUsed = ['common']
 
 const getFileName = (filePath) => {
   const { name } = path.parse(filePath)
   return name
 }
 
+// 递归分析组件间的依赖关系
+const relationParser = (relation, componentName, relationNew) => {
+  const keys = Object.keys(relation)
+  if (keys.includes(componentName)) {
+    relationNew.push(...relation[componentName])
+    relation[componentName].forEach((key) => {
+      relationParser(relation, key, relationNew)
+    })
+  } else {
+    return
+  }
+}
+
+// 获取组件间的依赖关系
+const getComponentRelation = (srcPath) => {
+  const components = fs.readdirSync(srcPath)
+  const componentRelation = {}
+  components.forEach((item) => {
+    // 排除公用的文件夹
+    if (commonUsed.includes(item)) return
+    const filePath = `${srcPath}/${item}/${item}.json`
+    const fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    // 排除没有依赖关系的组件
+    if (!fileData.usingComponents || Object.keys(fileData.usingComponents).length === 0) return
+    // 获取引用的各个组件的key 值
+    const componentKeys = Object.keys(fileData.usingComponents)
+    // 获取被引用的各个组件的名字
+    const componentsUsedCurrent = componentKeys.map((key) =>
+      getFileName(fileData.usingComponents[key])
+    )
+    componentRelation[item] = [...componentsUsedCurrent]
+  })
+  const componentRelationNew = {}
+  Object.keys(componentRelation).forEach((item) => {
+    componentRelationNew[item] = [...componentRelation[item]]
+    relationParser(componentRelation, item, componentRelationNew[item])
+  })
+  // 数据去重
+  Object.keys(componentRelationNew).forEach((item) => {
+    componentRelationNew[item] = Array.from(new Set(componentRelationNew[item]))
+  })
+  console.log('pakizheng component', componentRelationNew)
+  return componentRelationNew
+}
+
 let componentTransform = []
 
 function componentsShaking(components) {
-  const commonUsed = ['common']
   if (!components) {
     console.log('components 是必传参数，请检查对否有传入符合规定的参数')
     return through.obj({ defaultEncoding: 'utf8' }, function componentShaking(
@@ -26,7 +73,7 @@ function componentsShaking(components) {
     return {
       srcPath,
       distPath,
-      componentRelation: {},
+      componentRelation: getComponentRelation(srcPath),
       componentsUsed: [...commonUsed],
     }
   })
@@ -43,20 +90,8 @@ function componentsShaking(components) {
       // 如果文件里面含有 usingComponents 才进行处理
       if (fileData.usingComponents) {
         componentTransform.forEach((item) => {
-          // 如果路径包含组件库的绝对路径，说明此时的 json 文件是组件库的 json 文件，进行组件库中文件的引用关联处理
-          if (filePath.includes(item.srcPath)) {
-            // 获取当前文件的信息
-            const componentName = getFileName(filePath)
-            // 获取引用的各个组件的key 值
-            const componentKeys = Object.keys(fileData.usingComponents)
-            if (componentKeys.length === 0) return
-            // 获取被引用的各个组件的名字
-            const componentsUsedCurrent = componentKeys.map((key) =>
-              getFileName(fileData.usingComponents[key])
-            )
-            item.componentRelation[componentName] = [...componentsUsedCurrent]
-          } else {
-            // 对于非组件库的 json 文件，对引用的组件进行路径分析，如果是组件库组件，提取组件的名字，否则不做处理
+          // 对于非组件库的 json 文件，对引用的组件进行路径分析，如果是组件库组件，提取组件的名字，否则不做处理
+          if (!filePath.includes(item.srcPath)) {
             const componentKeys = Object.keys(fileData.usingComponents)
             const { dir } = path.parse(filePath)
             componentKeys.forEach((key) => {
@@ -79,7 +114,7 @@ function componentsShaking(components) {
   })
 }
 
-componentsShaking.delete = () => {
+componentsShaking.shaking = () => {
   // 编译结束之后对已经使用的组件，添加上关联的组件
   componentTransform.forEach((item) => {
     item.componentsUsed.forEach((itemUsed) => {
@@ -89,19 +124,17 @@ componentsShaking.delete = () => {
     })
     item.componentsUsed = Array.from(new Set(item.componentsUsed))
   })
-  // 删除多余的文件夹
+
+  // 删除旧的组件库文件夹，拷贝用到的组件到组件库文件夹中
   componentTransform.forEach((item) => {
-    const componentsAll = fs.readdirSync(item.distPath)
-    const componentUnUsed = []
-    componentsAll.forEach((i) => {
-      if (!item.componentsUsed.includes(i)) {
-        componentUnUsed.push(`${item.distPath}/${i}`)
-      }
-    })
-    componentUnUsed.forEach((itemUnUsed) => {
-      if (fs.statSync(itemUnUsed)) {
-        fs.rmdirSync(itemUnUsed, { recursive: true })
-      }
+    if (fs.statSync(item.distPath)) {
+      const componentsOldAll = fs.readdirSync(item.distPath)
+      componentsOldAll.forEach((oldComponent) => {
+        fs.rmdirSync(`${item.distPath}/${oldComponent}`, { recursive: true })
+      })
+    }
+    item.componentsUsed.forEach((newComponent) => {
+      fsExtra.copy(`${item.srcPath}/${newComponent}`, `${item.distPath}/${newComponent}`)
     })
   })
 }
